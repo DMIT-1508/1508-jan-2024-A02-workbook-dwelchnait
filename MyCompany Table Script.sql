172c29e361c884a0f0e310a8a406cc12140075fb
Don Welch <dwelch@nait.ca>
2024-01-30 10:53:22 -0700

    demo Drop Table and Foreign Keys

--- MyCompany Table Script.sql
+++ MyCompany Table Script.sql
@@ -1,3 +1,7 @@
+-- the command to create a database is: CREATE DATABASE databasename
+-- CREATE DATABASE MyCompany
+
+
 -- the use command will indicate which item to use
 --    at this point in time
 -- use databasename, will switch to the named database
@@ -16,6 +20,18 @@
 use MyCompany
 go
 
+-- you can drop tables within a create table script so that
+--		the create script will runs without errors
+-- order of dropping is IMPORTANT
+-- foreign key tables MUST be dropped BEFORE the primary key table
+--    referred to as child before parent
+drop table Orders
+drop table Customers
+go
+
+-- tables MUST be created in the order of parent to child
+--		thus primary keys NEED to exist BEFORE they can be used
+--		as a foreign key
 -- each create table MUST be in its own batch
 CREATE TABLE Customers
 (
@@ -24,5 +40,23 @@ CREATE TABLE Customers
 	LastName varchar(50) NOT NULL,
 	FirstName varchar(20) NOT NULL,
 	Phone varchar(14) NULL
+)
+go
+-- it is foreign keys that form the relationships between
+--		tables
+-- a foreign key on a table needs a constraint to indicate
+--		that it is the field which the DBMS is to use
+--		in managing FKey relationships
+CREATE TABLE Orders
+(
+	OrderNumber int IDENTITY (5,1) NOT NULL
+		CONSTRAINT PK_Orders_OrderNumber primary key,
+	OrderDate datetime NOT NULL,
+	SubTotal money NOT NULL,
+	GST money NOT NULL,
+	Total money NOT NULL,
+	CustomerNumber int NOT NULL
+		CONSTRAINT FK_OrdersCustomers_CustomerNumber foreign key
+			references Customers(CustomerNumber)
 )
 go
